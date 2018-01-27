d88d51d0e04efcfe8e970cfa2ed659587ac374ad
gradyw <grady.whelan@gmail.com>
2018-01-26 16:03:11 -0800

    Arm and Updated Elevator

---
+++ Robot2018/src/org/usfirst/frc2813/Robot2018/subsystems/Arm.java
@@ -0,0 +1,26 @@
+package org.usfirst.frc2813.Robot2018.subsystems;
+
+import org.usfirst.frc2813.Robot2018.RobotMap;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class Arm extends Subsystem {
+	public final SpeedController speedController1 = RobotMap.armSpeedController1;
+	public final Encoder encoder1 = RobotMap.armQuadratureEncoder1;
+	public final DigitalInput digitalInput1 = RobotMap.armDigitalInput1;
+
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+	@Override
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+}
+
