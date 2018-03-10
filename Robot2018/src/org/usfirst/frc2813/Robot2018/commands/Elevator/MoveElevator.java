d4fab362bbb65df09c3c8322b0ceaa412581d9ff
Michael Taylor <mike.taylor@ubnt.com>
2018-03-10 14:01:12 -0800

    WIP on refactoring Elevator control.

--- Robot2018/src/org/usfirst/frc2813/Robot2018/commands/Elevator/MoveElevator.java
+++ Robot2018/src/org/usfirst/frc2813/Robot2018/commands/Elevator/MoveElevator.java
@@ -5,6 +5,7 @@ import edu.wpi.first.wpilibj.command.Command;
 
 import org.usfirst.frc2813.Robot2018.Direction;
 import org.usfirst.frc2813.Robot2018.Robot;
+import org.usfirst.frc2813.Robot2018.TalonState;
 import org.usfirst.frc2813.Robot2018.subsystems.Elevator;
 
 /**
@@ -14,98 +15,78 @@ import org.usfirst.frc2813.Robot2018.subsystems.Elevator;
  * Position to move to that position in inches above floor.
  */
 public class MoveElevator extends Command {
-	private boolean halted;
-	private Direction direction;
-	private double position;
-    private boolean positionMode;  // true if we are moving to a position
-                                   // false if we move by direction and speed
-    private final Elevator elevator;
+	// Desired state/action
+	private final TalonState state;
+	// direction, only valid if we are moving at a constant speed
+	private final Direction direction;
+	// position, only valid if we are moving to a set position
+	private final double positionInInches;
+	/*
+	 * Hold the current position
+	 */
 	public MoveElevator() {
-		this.elevator = Robot.elevator;
-		halted = true;
+		this.state = TalonState.HOLDING_POSITION;
+		this.direction = Direction.NEUTRAL; // Not used
+		this.positionInInches = 0; // Not used
 		requires(Robot.elevator);
 	}
-
+	/*
+	 * Move in a particular direction until stopped, hold the velocity
+	 */
 	public MoveElevator(Direction direction) {
-		this.elevator = Robot.elevator;
-		this.direction = direction;
-        positionMode = false;
-		halted = false;
+		this.state = TalonState.MOVING;
+		this.direction = direction; 
+		this.positionInInches = 0; // Not used
 	}
-
-	public MoveElevator(double position) {
-		this.elevator = Robot.elevator;
-		this.position = position;
-        positionMode = true;
-		halted = false;
+	/*
+	 * Move to a specific position and hold the position
+	 */
+	public MoveElevator(double positionInInches) {
+		this.state = TalonState.SET_POSITION;
+		this.positionInInches = positionInInches;
+		this.direction = Direction.NEUTRAL; // Not used
 	}
-
 	// Called repeatedly when this Command is scheduled to run
-	//@Override
+	// @Override
 	protected void execute() {
-		if (halted) {
+		switch(state) {
+		case HOLDING_POSITION:
 			Robot.elevator.holdCurrentPosition();
-		}
-		else {
-            if (positionMode) {
-                direction = Robot.elevator.readPosition() > position ? Direction.DOWN : Direction.UP;
-            }
-            Robot.elevator.move(direction, position);
+			break;
+		case MOVING:
+			Robot.elevator.move(direction);
+			break;
+		case SET_POSITION:
+			Robot.elevator.setPosition(positionInInches);
+			break;
+		case DISABLED:
+			Robot.elevator.disable();
+			break;
+		default:
+			throw new IllegalArgumentException("Unsupported state: " + state);
 		}
 	}
-
 	// Make this return true when this Command no longer needs to run execute()
 	//@Override
 	protected boolean isFinished() {
-        double currentPosition;
-        double targetPosition;
-		boolean done;
-
-		if (halted) return true;
-
-		currentPosition = elevator.readPosition();
-
-		/*
-		 * NB: When holding "up" or "down" buttons we are not
-		 * in positionMode, and targetPosition is set to 0 or HEIGHT
-		 * and limit switches will stop motion.
-		 * One area of concern is that any offset in encoder values
-		 * or inaccuracy will result in never reaching done state
-		 * even if the soft limit is hit or the limit switch triggers.
-		 * "done" should also be checking the controller to see if
-		 * either soft or hard limit is it.  Then we need to adjust
-		 * if we hit a physical limit without hitting the absolute value, 
-		 * HEIGHT or 0.
-		 */
-        if (positionMode) {
-            targetPosition = position;
-        }
-        else if (direction == Direction.DOWN) {
-            targetPosition = 0;
-        }
-        else {
-            targetPosition = elevator.HEIGHT;
-        }
-
-		if (direction == Direction.DOWN) {
-            done = currentPosition <= targetPosition;
-		}
-        else {
-            done = currentPosition >= targetPosition;
-        }
-		System.out.println("MoveElevator [PositionMode=" + positionMode + ", Direction=" + direction + ", Target=" + targetPosition + ", Position=" + currentPosition + "]");
-		if (done) {
-			System.out.println("MoveElevator_FINISHED");
-			halted = true;
-			elevator.holdCurrentPosition();
+		switch(state) {
+		case HOLDING_POSITION:
+			// "done" means no more action to take
 			return true;
+		case MOVING:
+			// It's moving at a velocity, with PID.  The command is done.
+			return true;
+		case SET_POSITION:
+			// It's moving to a position, holding with PID.  The command is done.
+			return true;
+		case DISABLED:
+			return true;
+		default:
+			throw new IllegalArgumentException("Unsupported state: " + state);
 		}
-		return false;
 	}
-
 	@Override
 	protected void end() {}
-
 	@Override
 	protected void interrupted() {}
 }
