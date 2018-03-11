7144ec04a31ab7894d0845661940541b3f7f3b96
Jack Taylor <jtaylor@emeraldcave.net>
2018-03-10 20:26:29 -0800

    Changes to values to match the setElevatorSpeed class.

--- Robot2018/src/org/usfirst/frc2813/Robot2018/commands/Arm/SetArmSpeed.java
+++ Robot2018/src/org/usfirst/frc2813/Robot2018/commands/Arm/SetArmSpeed.java
@@ -1,26 +1,31 @@
 package org.usfirst.frc2813.Robot2018.commands.Arm;
 
 import org.usfirst.frc2813.Robot2018.subsystems.Arm;
-
+import org.usfirst.frc2813.Robot2018.subsystems.Elevator;
+import org.usfirst.frc2813.Robot2018.Robot;
 import edu.wpi.first.wpilibj.command.InstantCommand;
 
 /**
- * Command to set speed of elevator.
- * FIXME! Does not take effect without moveElevator - should we change this?
+ * Command to set speed of arm.
+ * FIXME! Does not take effect without moveArm - should we change this?
  */
 public class SetArmSpeed extends InstantCommand {
-	double speed;
+	private double newSpeedInchesPerSecond;
+    private final Arm arm;
+
 
 	/**
-	 * Set the speed of the elevator
+	 * Set the speed of the arm
 	 * @param speed in feet per second
 	 */
-	public SetArmSpeed(double speed) {
-		this.speed=speed;
+	public SetArmSpeed(double newSpeedInchesPerSecond) {
+		this.arm = Robot.arm;
+		this.newSpeedInchesPerSecond = newSpeedInchesPerSecond;
+
 	}
 
 	// Called once when the command executes
 	protected void initialize() {
-		Arm.setArmSpeed(speed);
+		arm.setArmSpeed(newSpeedInchesPerSecond); 
 	}
 }
