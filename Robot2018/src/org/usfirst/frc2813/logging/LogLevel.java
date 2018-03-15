b4ba9ac1c71d3f411921c47babe6ccb8225a20b1
adrianmgg <2adrian2@gmail.com>
2018-03-15 11:14:49 -0700

    moved logging package

--- Robot2018/src/org/usfirst/frc2813/logging/LogLevel.java
+++ Robot2018/src/org/usfirst/frc2813/logging/LogLevel.java
@@ -1,4 +1,4 @@
-package logging;
+package org.usfirst.frc2813.logging;
 
 import java.util.Arrays;
 
--- Robot2018/src/org/usfirst/frc2813/logging/Logger.java
+++ Robot2018/src/org/usfirst/frc2813/logging/Logger.java
@@ -1,4 +1,4 @@
-package logging;
+package org.usfirst.frc2813.logging;
 
 import java.util.ArrayList;
 import java.util.Arrays;
