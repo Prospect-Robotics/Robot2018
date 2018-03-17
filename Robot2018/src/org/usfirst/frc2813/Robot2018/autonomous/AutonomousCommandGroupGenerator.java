981332ce4342c9495e2b413a15f31fd63da242ce
Josh Grossman <jgrossman@apple.com>
2018-03-17 11:55:15 -0700

    logging in autonomous

--- Robot2018/src/org/usfirst/frc2813/Robot2018/autonomous/AutonomousCommandGroupGenerator.java
+++ Robot2018/src/org/usfirst/frc2813/Robot2018/autonomous/AutonomousCommandGroupGenerator.java
@@ -49,15 +49,18 @@ public class AutonomousCommandGroupGenerator {
 
 		if (RobotMap.gameData.getScale() == Direction.OFF) {
 			// there is no game data. Cross the auto line
+			Logger.info("Autonomous: no game data");
 			autoCmdList.driveForward(LengthUOM.Feet.create(5));
 			return;
 		}
 		
 		// These return immediately and can happen while we drive
+		Logger.info("Autonomous: set default elevator/arm position");
 		autoCmdList.elevatorMoveToPosition(switchHeight); // min needed and max safe during drive
 		autoCmdList.raiseArm();
 
 		if (position == RobotMap.gameData.getScale()) {
+			Logger.info("Autonomous: robot and scale on same side");
 			// we are on the same side as the scale. Leave switch for team mates
 			autoCmdList.driveForward(LengthUOM.Feet.create(24));
 			autoCmdList.elevatorMoveToPosition(scaleHeight);
@@ -66,6 +69,7 @@ public class AutonomousCommandGroupGenerator {
 		}
 		else if (position != Direction.CENTER) {
 			// from far side we cross over between switch and scale and place block on scale
+			Logger.info("Autonomous: robot and scale on opposite side");
 			autoCmdList.driveForward(LengthUOM.Feet.create(14));
 			autoCmdList.turnRight(90 * directionBias);
 			autoCmdList.driveForward(LengthUOM.Feet.create(15));
@@ -80,6 +84,7 @@ public class AutonomousCommandGroupGenerator {
 			 // allows left->right and right->left to share code
 			directionBias = (position == Direction.LEFT) ? 1 : -1;
 
+			Logger.info("Autonomous: robot in center position");
 			autoCmdList.driveForward(LengthUOM.Inches.create(6)); // enough to turn
 			autoCmdList.turnLeft(45 * directionBias);
 			autoCmdList.driveForward(LengthUOM.Feet.create(6)); // diagonally from start to far side of near switch
