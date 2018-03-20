bccf6fdc99bb6f7a1b5ba9886cc94eb75e7eccad
Josh Grossman <jgrossman@apple.com>
2018-03-19 17:03:21 -0700

    Add basic plumbing for emulation to subsytem parent class

--- Robot2018/src/org/usfirst/frc2813/Robot2018/subsystems/GearheadsSubsystem.java
+++ Robot2018/src/org/usfirst/frc2813/Robot2018/subsystems/GearheadsSubsystem.java
@@ -11,5 +11,28 @@ public abstract class GearheadsSubsystem extends Subsystem {
 	static {
 		Logger.addMe();
 	}
+	private boolean _isEmulated = false;
 	public boolean encoderFunctional = true;
+
+	/**
+	 * Is the subsystem emulated?
+	 * @return true/false
+	 */
+	public boolean isEmulated() {
+		return _isEmulated;
+	}
+
+	/**
+	 * enable emulation mode. Override to initialize emulation state
+	 */
+	public void enableEmulator() {
+		_isEmulated = true;
+	}
+
+	/**
+	 * disable emulation mode
+	 */
+	public void disableEmulator() {
+		_isEmulated = false;
+	}
 }
