d4332ec86029cae0bf1184bfe66451c008ffaa5a
Michael Taylor <mike.taylor@ubnt.com>
2018-03-21 00:19:54 -0700

    Add a command to wait for a hard limit to be reached, for use in calibration sequences for arm and elevator.  Do not "require" the motor in question, but allow any synchronous command to continue as it's possible that command is moving us to the hard limit.  Another way this can be used is to create a "when hit" triggered asynchronous command sequence.

---
+++ Robot2018/src/org/usfirst/frc2813/Robot2018/commands/motor/MotorWaitForHardLimitSwitch.java
@@ -0,0 +1,41 @@
+package org.usfirst.frc2813.Robot2018.commands.motor;
+
+import org.usfirst.frc2813.Robot2018.subsystems.motor.Motor;
+import org.usfirst.frc2813.logging.LogType;
+import org.usfirst.frc2813.logging.Logger;
+import org.usfirst.frc2813.units.values.Length;
+import org.usfirst.frc2813.units.Direction;
+
+/**
+ * Wait for a motor system to arrive at position, to within +/- allowableError
+ */
+public class MotorWaitForHardLimitSwitch extends MotorCommand {
+	private final Direction switchDirection;
+
+	public MotorWaitForHardLimitSwitch(Motor motor, Direction switchDirection) {
+		super(motor, false); // NB: IF this is running asynchronously, we don't interrupt the other command...
+		this.switchDirection = switchDirection;
+	}
+
+	@Override
+	protected void initialize() {
+		super.initialize();
+		// NB: If the motor is moving to a position (relative or absolute)
+		if(!isFinished()) {
+			Logger.printFormat(LogType.INFO,"%s waiting for %s to reach %s hard limit switch.", this, motor, switchDirection);
+		} else {
+			Logger.printFormat(LogType.INFO,"%s NOT waiting pointlessly for %s to reach %s hard limit switch, it's already there.", this, motor, switchDirection);
+		}
+		setInterruptible(true);
+	}
+
+	@Override
+	protected boolean isFinished() {
+		// NB: If there's no target position (absolute or relative), this will always return true.
+		return motor.getCurrentLimitSwitchStatus(switchDirection);
+	}
+
+    public String toString() {
+        return getClass().getSimpleName() + "(" + motor + "," + switchDirection + ")";
+    }
+}
