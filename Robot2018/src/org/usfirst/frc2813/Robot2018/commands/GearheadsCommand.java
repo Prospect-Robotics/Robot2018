e2651ff0c1fc1acdcf46d567bdaf040ff2163c31
Michael Taylor <mike.taylor@ubnt.com>
2018-03-21 19:21:26 -0700

    Add a tracing debug level and apply it to all operations, so that we can trace auto execution should a command get stuck.

--- Robot2018/src/org/usfirst/frc2813/Robot2018/commands/GearheadsCommand.java
+++ Robot2018/src/org/usfirst/frc2813/Robot2018/commands/GearheadsCommand.java
@@ -1,5 +1,6 @@
 package org.usfirst.frc2813.Robot2018.commands;
 
+import org.usfirst.frc2813.logging.LogType;
 import org.usfirst.frc2813.logging.Logger;
 
 import edu.wpi.first.wpilibj.command.Command;
@@ -8,6 +9,20 @@ import edu.wpi.first.wpilibj.command.Command;
  * This class contains code common to all Subsystems
  */
 public abstract class GearheadsCommand extends Command {
+	
+	private static final LogType TRACING_LOG_LEVEL = LogType.DEBUG;
+	@Override
+	protected void end() {
+		Logger.print(TRACING_LOG_LEVEL, this + " in end.");
+		super.end();
+	}
+
+	@Override
+	public synchronized void cancel() {
+		Logger.print(TRACING_LOG_LEVEL, this + " in cancel.");
+		super.cancel();
+	}
+
 	private boolean isDefaultCommand = false;
 	static {
 		Logger.addMe();
@@ -28,25 +43,29 @@ public abstract class GearheadsCommand extends Command {
 	public GearheadsCommand(String name, double timeout) {
 		super(name, timeout);
 	}
-	
+
 	protected boolean isFinished() {
-		Logger.debug(this + " in isFinished");
+		if(!isDefaultCommand()) {
+			Logger.print(TRACING_LOG_LEVEL, this + " in isFinished");
+		}
 		return false;
 	}
 
 	// @Override
 	protected void execute() {
-		Logger.debug(this + " in execute");
+		if(!isDefaultCommand()) {
+			Logger.print(TRACING_LOG_LEVEL, this + " in execute");
+		}
 	}
-	
+
 	// @Override
 	protected void initialize() {
-		Logger.debug(this + " in initialize");
+		Logger.print(TRACING_LOG_LEVEL, this + " in initialize");
 	}
-	
+
 	// @Override
 	protected void interrupted() {
-		Logger.debug(this + " interrupted");
+		Logger.print(TRACING_LOG_LEVEL, this + " interrupted");
 	}
 	
 	public void setIsDefaultCommand(boolean isDefaultCommand) {
