a2d3510856194c240d030a7fe071ed659be945d8
Michael Taylor <mike.taylor@ubnt.com>
2018-03-23 15:58:17 -0700

    A little hack to allow commands to compile when the Motor is not a Subsystem subclass.

--- Robot2018/src/org/usfirst/frc2813/Robot2018/commands/motor/AbstractMotorCommand.java
+++ Robot2018/src/org/usfirst/frc2813/Robot2018/commands/motor/AbstractMotorCommand.java
@@ -3,6 +3,8 @@ package org.usfirst.frc2813.Robot2018.commands.motor;
 import org.usfirst.frc2813.Robot2018.commands.GearheadsCommand;
 import org.usfirst.frc2813.Robot2018.subsystems.motor.Motor;
 
+import edu.wpi.first.wpilibj.command.Subsystem;
+
 /**
  * Abstract base class for motor commands.
  */
@@ -10,8 +12,9 @@ public abstract class AbstractMotorCommand extends GearheadsCommand {
 	protected final Motor motor;
 	public AbstractMotorCommand(Motor subsystem, boolean requiresSubsystem) {
 		this.motor = subsystem;
-		if(requiresSubsystem) {
-			requires(subsystem);
+		// NB: This evil is required to get a compile when Motor is built for standalone mode
+		if(requiresSubsystem && Subsystem.class.isInstance(subsystem)) {
+			requires((Subsystem)(Object)subsystem);
 		}
 	}
 	
