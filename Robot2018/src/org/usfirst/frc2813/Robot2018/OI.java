5914b61aee48aedc39ca41534ad7ef406c773fba
Michael Taylor <mike.taylor@ubnt.com>
2018-03-23 22:59:03 -0700

    Button 10 is now calibrate for arm and then elevator (order is important, or you will slam the arm into the ground).  I have commented out test behaviors for buttons 6-9 that I am using for PID tuning for the six PID configurations I'm working on.

--- Robot2018/src/org/usfirst/frc2813/Robot2018/OI.java
+++ Robot2018/src/org/usfirst/frc2813/Robot2018/OI.java
@@ -5,8 +5,13 @@ import java.util.function.BiConsumer;
 import org.usfirst.frc2813.Robot2018.commands.ToggleCompressor;
 import org.usfirst.frc2813.Robot2018.commands.drivetrain.DriveTrainOIDriveSync;
 import org.usfirst.frc2813.Robot2018.commands.intake.IntakeSpinSync;
+import org.usfirst.frc2813.Robot2018.commands.motor.MotorCalibrateSensorAsync;
 import org.usfirst.frc2813.Robot2018.commands.motor.MotorMoveInDirectionSync;
-import org.usfirst.frc2813.Robot2018.commands.motor.MotorPIDTest;
+import org.usfirst.frc2813.Robot2018.commands.motor.MotorMoveToAbsolutePositionAsync;
+import org.usfirst.frc2813.Robot2018.commands.motor.MotorPositionPIDTest;
+import org.usfirst.frc2813.Robot2018.commands.motor.MotorVelocityPIDTest;
+import org.usfirst.frc2813.Robot2018.commands.motor.MotorWaitForHardLimitSwitchSync;
+import org.usfirst.frc2813.Robot2018.commands.motor.MotorWaitForTargetPositionSync;
 import org.usfirst.frc2813.Robot2018.commands.solenoid.SolenoidToggleStateInstant;
 import org.usfirst.frc2813.Robot2018.subsystems.user.RoboRIOUserButton;
 import org.usfirst.frc2813.units.Direction;
@@ -14,6 +19,8 @@ import org.usfirst.frc2813.units.uom.LengthUOM;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
+import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.command.TimedCommand;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -73,16 +80,50 @@ public class OI {
 		 *
 		 */
 		buttonPanel = new Joystick(0);
+		CommandGroup calibration = new CommandGroup();
+		calibration.addSequential(new MotorCalibrateSensorAsync(Robot.arm, Direction.REVERSE));
+		calibration.addSequential(new MotorWaitForHardLimitSwitchSync(Robot.arm, Direction.REVERSE));
+		calibration.addSequential(new MotorCalibrateSensorAsync(Robot.elevator, Direction.REVERSE));
+		calibration.addSequential(new MotorWaitForHardLimitSwitchSync(Robot.elevator, Direction.REVERSE));
 
 		new JoystickButton(buttonPanel, 1).whileHeld(new IntakeSpinSync(Robot.intake, Direction.IN));
 		new JoystickButton(buttonPanel, 2).whileHeld(new IntakeSpinSync(Robot.intake, Direction.OUT));
 		new JoystickButton(buttonPanel, 4).whileHeld(new MotorMoveInDirectionSync(Robot.elevator, Direction.DOWN));
 		new JoystickButton(buttonPanel, 5).whileHeld(new MotorMoveInDirectionSync(Robot.elevator, Direction.UP));
-		new JoystickButton(buttonPanel, 6).whenPressed(new SolenoidToggleStateInstant(Robot.climbingBar));
-		new JoystickButton(buttonPanel, 7).whenPressed(new SolenoidToggleStateInstant(Robot.driveTrain.getGearShiftSolenoid()));
-		new JoystickButton(buttonPanel, 8).whenPressed(new SolenoidToggleStateInstant(Robot.ratchet));
-		new JoystickButton(buttonPanel, 9).whenPressed(new SolenoidToggleStateInstant(Robot.jaws));
-		new JoystickButton(buttonPanel, 10).whileHeld(new MotorPIDTest(Robot.elevator, LengthUOM.Inches.create(5), LengthUOM.Inches.create(30)));
+		if(true) { // NB: The other settings are for Mike's debugging of PID vales on Arm and Elevator and are disabled by default.		
+				new JoystickButton(buttonPanel, 6).whenPressed(new SolenoidToggleStateInstant(Robot.climbingBar));
+				new JoystickButton(buttonPanel, 7).whenPressed(new SolenoidToggleStateInstant(Robot.driveTrain.getGearShiftSolenoid()));
+				new JoystickButton(buttonPanel, 8).whenPressed(new SolenoidToggleStateInstant(Robot.ratchet));
+				new JoystickButton(buttonPanel, 9).whenPressed(new SolenoidToggleStateInstant(Robot.jaws));
+		} else {
+			CommandGroup armDemo = new CommandGroup();
+			armDemo.addSequential(new MotorMoveToAbsolutePositionAsync(Robot.arm, Robot.arm.getConfiguration().getNativeDisplayLengthUOM().create(30)));
+			armDemo.addSequential(new MotorWaitForTargetPositionSync(Robot.arm, Robot.arm.getConfiguration().getNativeDisplayLengthUOM().create(5)));
+			armDemo.addSequential(new TimedCommand(3));
+			armDemo.addSequential(new MotorMoveToAbsolutePositionAsync(Robot.arm, Robot.arm.getConfiguration().getNativeDisplayLengthUOM().create(45)));
+			armDemo.addSequential(new MotorWaitForTargetPositionSync(Robot.arm, Robot.arm.getConfiguration().getNativeDisplayLengthUOM().create(5)));
+			armDemo.addSequential(new TimedCommand(3));
+			armDemo.addSequential(new MotorMoveToAbsolutePositionAsync(Robot.arm, Robot.arm.getConfiguration().getNativeDisplayLengthUOM().create(60)));
+			armDemo.addSequential(new MotorWaitForTargetPositionSync(Robot.arm, Robot.arm.getConfiguration().getNativeDisplayLengthUOM().create(5)));
+			armDemo.addSequential(new TimedCommand(3));
+			armDemo.addSequential(new MotorMoveToAbsolutePositionAsync(Robot.arm, Robot.arm.getConfiguration().getNativeDisplayLengthUOM().create(75)));
+			armDemo.addSequential(new MotorWaitForTargetPositionSync(Robot.arm, Robot.arm.getConfiguration().getNativeDisplayLengthUOM().create(5)));
+			armDemo.addSequential(new TimedCommand(3));
+			armDemo.addSequential(new MotorMoveToAbsolutePositionAsync(Robot.arm, Robot.arm.getConfiguration().getNativeDisplayLengthUOM().create(90)));
+			armDemo.addSequential(new MotorWaitForTargetPositionSync(Robot.arm, Robot.arm.getConfiguration().getNativeDisplayLengthUOM().create(5)));
+			armDemo.addSequential(new TimedCommand(3));
+			armDemo.addSequential(new MotorMoveToAbsolutePositionAsync(Robot.arm, Robot.arm.getConfiguration().getNativeDisplayLengthUOM().create(105)));
+			armDemo.addSequential(new MotorWaitForTargetPositionSync(Robot.arm, Robot.arm.getConfiguration().getNativeDisplayLengthUOM().create(5)));
+			armDemo.addSequential(new TimedCommand(3));
+			
+			new JoystickButton(buttonPanel, 6).whileHeld(new MotorPositionPIDTest(Robot.arm, Robot.arm.getConfiguration().getNativeDisplayLengthUOM().create(15), Robot.arm.getConfiguration().getNativeDisplayLengthUOM().create(110)));
+			new JoystickButton(buttonPanel, 7).whileHeld(new MotorPositionPIDTest(Robot.elevator, LengthUOM.Inches.create(5), LengthUOM.Inches.create(30)));
+			//	new JoystickButton(buttonPanel, 8).whileHeld(new MotorVelocityPIDTest(Robot.arm));
+			new JoystickButton(buttonPanel, 8).whenPressed(armDemo);
+			new JoystickButton(buttonPanel, 9).whileHeld(new MotorVelocityPIDTest(Robot.elevator));
+			new JoystickButton(buttonPanel, 9).whileHeld(new MotorMoveToAbsolutePositionAsync(Robot.elevator, LengthUOM.Inches.create(6)));
+		}
+		new JoystickButton(buttonPanel, 10).whenPressed(calibration);
 		new JoystickButton(buttonPanel, 11).whileHeld(new MotorMoveInDirectionSync(Robot.arm, Direction.IN));
 		new JoystickButton(buttonPanel, 12).whileHeld(new MotorMoveInDirectionSync(Robot.arm, Direction.OUT));
 
