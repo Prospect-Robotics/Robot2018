66463a9337e9a8e3bfc41924c3682191de3c8fdf
jitb0824 <jtaylor@emeraldcave.net>
2018-04-03 16:50:30 -0700

    Fix for encoder values.  WPI says it accounts for encoder mode 4x but only seems to be treating it like 2x.

--- Robot2018/src/org/usfirst/frc2813/Robot2018/subsystems/drivetrain/DriveTrain.java
+++ Robot2018/src/org/usfirst/frc2813/Robot2018/subsystems/drivetrain/DriveTrain.java
@@ -42,7 +42,7 @@ public class DriveTrain extends GearheadsSubsystem {
     public static final double ENCODER_TO_DRIVE_SCALE = 3;
     public static final double ENCODER_PULSES_PER_ENCODER_REVOLUTION = 256;
     public static final double ENCODER_PULSES_PER_WHEEL_REVOLUTION = ENCODER_PULSES_PER_ENCODER_REVOLUTION * ENCODER_TO_DRIVE_SCALE;
-    public static final double INCHES_PER_ENCODER_PULSE = WHEEL_CIRCUMFERENCE_INCHES / ENCODER_PULSES_PER_WHEEL_REVOLUTION;
+    public static final double INCHES_PER_ENCODER_PULSE = WHEEL_CIRCUMFERENCE_INCHES / ENCODER_PULSES_PER_WHEEL_REVOLUTION * 2.0;
 
     private final Gyro gyro;
     private final SpeedController speedControllerPort;
